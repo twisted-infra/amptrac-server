227a4e0f1fa24a03013cd8b9937457d133dad362
Allen Short <washort@twistedmatrix.com>
2012-04-07 22:19:32 -0400

    Add JSON-RPC support.

---
+++ frack/responder.py
@@ -0,0 +1,37 @@
+from twisted.protocols import amp
+
+class FetchTicket(amp.Command):
+    arguments = [('id', amp.Integer())]
+    response = [('id', amp.Integer()),
+                ('type', amp.Unicode()),
+                ('time', amp.Integer()),
+                ('component', amp.Unicode()),
+                ('priority', amp.Unicode()),
+                ('owner', amp.Unicode()),
+                ('reporter', amp.Unicode()),
+                ('cc', amp.Unicode()),
+                ('status', amp.Unicode()),
+                ('resolution', amp.Unicode()),
+                ('summary', amp.Unicode()),
+                ('description', amp.Unicode()),
+                ('keywords', amp.Unicode()),
+                ('changes', amp.AmpList([('time', amp.Integer()),
+                                         ('author', amp.Unicode()),
+                                         ('field', amp.Unicode()),
+                                         ('oldvalue', amp.Unicode()),
+                                         ('newvalue', amp.Unicode())]))]
+
+
+
+class AMPFace(amp.BoxDispatcher, amp.CommandLocator):
+
+    def __init__(self, store):
+        amp.BoxDispatcher.__init__(self, self)
+        self.store = store
+
+
+    @FetchTicket.responder
+    def fetchTicket(self, id):
+        d = self.store.fetchTicket(id)
+        return d
+
--- frack/wiring.py
+++ frack/wiring.py
@@ -1,47 +1,16 @@
 # Copyright (c) Twisted Matrix Laboratories.
 # See LICENSE for details.
+import types
+import simplejson as json
 from twisted.protocols import amp
 from twisted.application.service import Service
 from twisted.internet.protocol import ServerFactory
 from twisted.internet.endpoints import serverFromString
+from twisted.web.resource import Resource
+from twisted.web.server import NOT_DONE_YET, Site
+from twisted.internet.defer import maybeDeferred
 from twisted.internet import reactor
-
-class FetchTicket(amp.Command):
-    arguments = [('id', amp.Integer())]
-    response = [('id', amp.Integer()),
-                ('type', amp.Unicode()),
-                ('time', amp.Integer()),
-                ('component', amp.Unicode()),
-                ('priority', amp.Unicode()),
-                ('owner', amp.Unicode()),
-                ('reporter', amp.Unicode()),
-                ('cc', amp.Unicode()),
-                ('status', amp.Unicode()),
-                ('resolution', amp.Unicode()),
-                ('summary', amp.Unicode()),
-                ('description', amp.Unicode()),
-                ('keywords', amp.Unicode()),
-                ('changes', amp.AmpList([('time', amp.Integer()),
-                                         ('author', amp.Unicode()),
-                                         ('field', amp.Unicode()),
-                                         ('oldvalue', amp.Unicode()),
-                                         ('newvalue', amp.Unicode())]))]
-
-
-
-class AMPFace(amp.BoxDispatcher, amp.CommandLocator):
-
-    def __init__(self, store):
-        amp.BoxDispatcher.__init__(self, self)
-        self.store = store
-
-
-    @FetchTicket.responder
-    def fetchTicket(self, id):
-        d = self.store.fetchTicket(id)
-        return d
-
-
+from frack.responder import AMPFace
 
 class AMPFactory(ServerFactory):
     def __init__(self, store):
@@ -70,9 +39,83 @@ class AMPService(Service):
 
 
 class JSONRPCService(Service):
-    pass
+    def __init__(self, port, store):
+        self.port = port
+        self.store = store
+
+
+    def startService(self):
+        self.endpoint = serverFromString(reactor, self.port)
+        self.root = JSONRPCFace(self.store)
+        self.site = Site(self.root)
+        self.endpoint.listen(self.site)
 
 
 
-class JSONRPCFace(object):
-    pass
+UNKNOWN_ERROR, UNHANDLED_ERROR_CODE = (-32603, -32601)
+
+class JSONRPCFace(Resource):
+
+    def __init__(self, store):
+        Resource.__init__(self)
+        self.ampface = AMPFace(store)
+
+
+    def _serialize(self, response, request):
+        request.write(json.dumps(response))
+        request.finish()
+
+
+    def _err(self, failure, request, qid):
+        return self._fail(request, UNKNOWN_ERROR,
+                          str(failure.value), qid)
+
+
+    def _fail(self, request, code, msg, qid):
+        self._serialize({"jsonrpc": "2.0",
+                         "error":
+                             {"code": code,
+                              "message": msg},
+                         "id": qid},
+                        request)
+
+
+    def _succeed(self, result, request, qid):
+        self._serialize({"jsonrpc": "2.0",
+                         "result": result,
+                         "id": qid},
+                        request)
+
+
+    def _locateResponder(self, name):
+        cd = self.ampface._commandDispatch
+        if name in cd:
+            commandClass, responderFunc = cd[name]
+            responderMethod = types.MethodType(
+                responderFunc, self.ampface, AMPFace)
+            return responderMethod
+
+    def render(self, request):
+        q = json.loads(request.content.read())
+        qid = q.get('id')
+        resp = self._locateResponder(q['method'])
+        if resp is None:
+            self._fail(request, UNHANDLED_ERROR_CODE,
+            "Unhandled Command: %r" % (q['method'],), qid)
+        else:
+            self._invoke(request, resp, q['params'], qid)
+        return NOT_DONE_YET
+
+
+    def _invoke(self, request, responder, params, qid):
+        d = maybeDeferred(responder, **dict([(k.encode('ascii'), v) for (k, v) in params.iteritems()]))
+        d.addCallback(self._succeed, request, qid)
+        d.addErrback(self._err, request, qid)
+        return d
+
+
+    def getChild(self, path, request):
+        if path == '':
+            return self
+        else:
+            return Resource.getChild(self, path, request)
